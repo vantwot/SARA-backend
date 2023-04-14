f427782ae181b32c5705704b0c7da97bcb9300f3
つばき_0x01 <60707399+tsubakijuju13@users.noreply.github.com>
2023-04-14 18:27:37 -0500

    Change database default config

--- core/settings.py
+++ core/settings.py
@@ -110,14 +110,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db-sara',
         'USER': 'tsubaki@db-sara',
         'PASSWORD': 'Univalle2023',
         'HOST': 'db-sara.postgres.database.azure.com',
         'PORT': '5432'
-    )
+    }
 }
 
 # Password validation
