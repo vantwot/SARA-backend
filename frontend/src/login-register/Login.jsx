2f9ecf3f7fd78c3561ded0dff9f70e01a807be17
KPOETA1 <geider.montano@correounivalle.edu.co>
2023-04-17 18:53:06 -0500

    Interfaz login usuario 1.0
    Interfaz para el inicio de sesión de usuario al sistema SARA

--- frontend/src/login-register/Login.jsx
+++ frontend/src/login-register/Login.jsx
@@ -1,47 +1,138 @@
-import React, {useState} from "react";
-import './logreg.css';
-
+import React, { useState } from "react";
+import "./logreg.css";
+import univalle from "./Univalle.svg.png";
 export const Login = (props) => {
-    const [code, setcode] = useState('');
-    const [pass, setPass] = useState('');
+  const [code, setcode] = useState("");
+  const [pass, setPass] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        let check = await clientCredentials(code,pass);
-        
-        if (check){
-            console.log("asiogvasdovfsdapvaosv")
-            props.onFormSwitch('page');
-        }
-            
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let check = await clientCredentials(code, pass);
+
+    if (check) {
+      console.log("asiogvasdovfsdapvaosv");
+      props.onFormSwitch("http://localhost:3000/profile");
     }
+  };
+  //console.log(clientCredentials(code, pass));
 
-    return (
-        <div className="auth-form-container">
-            <h2>login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="code">code</label>
-                <input value={code} onChange={(e)=> setcode(e.target.value)} type="code" placeholder="yourcode" id="code" name="code" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e)=> setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit" >LOG IN</button>
-            </form>
-            <button className="link-btn">Don't have an account? Contact an administrator</button>
+  return (
+    <div className="background">
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossOrigin="anonymous"
+      />
+      <link rel="stylesheet" href="style.css" />
+      {/*--------------------- Main Container ------------------------*/}
+      <form onSubmit={handleSubmit}>
+      <div className="container d-flex justify-content-center align-items-center min-vh-100">
+        {/*--------------------- Login Container ------------------------*/}
+        <div className="row border rounded-5 p-3 bg-white shadow box-area" >
+          {/*------------------------- Left Box ---------------------------*/}
+          <div
+            className="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
+            style={{ background: "#ffffff" }}
+          >
+            <div className="featured-image mb-3">
+              <img
+                src={univalle}
+                className="img-fluid"
+                style={{ width: 250 }}
+                alt=""
+              />
+            </div>
+            <label
+              className="text-red fs-2"
+              style={{
+                fontFamily: '"Courier New", Courier, monospace',
+                fontWeight: 600,
+              }}
+            >
+              Bienvenido
+            </label>
+            <small
+              className="text-red text-wrap text-center"
+              style={{
+                width: "17rem",
+                fontFamily: '"Courier New", Courier, monospace',
+              }}
+            >
+              Recuerda si estas aqui eres de lo mejor por eso perteneces a la
+              mejor
+            </small>
+          </div>
+          {/*------------------ ------ Right Box --------------------------*/}
+          <div className="col-md-6 right-box">
+            <div className="row align-items-center">
+              <div className="header-text mb-4">
+                <h2>Sistema Avanzado de Registro Academico</h2>
+                <p>Estamos felices que estes de vuelta</p>
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  //pattern="[0-9]*"
+                  value={code}
+                  onChange={(e) => setcode(e.target.value)}
+                  id="code"
+                  name="code"
+                  type="code"
+                  className="form-control form-control-lg bg-light fs-6"
+                  placeholder="Codigo de identificacion"
+                />
+              </div>
+              <div className="input-group mb-1">
+                <input
+                  value={pass}
+                  onChange={(e) => setPass(e.target.value)}
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="form-control form-control-lg bg-light fs-6"
+                  placeholder="Contraseña"
+                />
+              </div>
+              <div className="input-group mb-5 d-flex justify-content-between">
+                <div className="forgot">
+                  <small>
+                    <a href="/recover" className="link-primary">
+                      Recuperar contraseña
+                    </a>
+                  </small>
+                </div>
+              </div>
+              <div className="input-group mb-3">
+                <button className="btn btn-lg btn-danger w-100 fs-6" type="submit">
+                  Iniciar
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+      </form>
+    </div>
+  );
+};
 
-async function clientCredentials(code,password){
-    let data  = await fetch(`https://saraendpoint.azurewebsites.net/user/`)
-    //use string literals
-    let dataJson = await data.json();
-    console.log(dataJson)
-    console.log("user name:",code,"pass: ", password)
-    for (let i = 0; i < dataJson.length; i++){
-        //console.log(`${i}`,dataJson[i].code,code,(dataJson[i].code).toString() === (code).toString())
-        //console.log(`${i}`,dataJson[i].name,password,dataJson[i].name === password);
-        if ((dataJson[i].code).toString() === code && (dataJson[i].name).toString() === password)
-            return true
-    }
-    return false;
+async function clientCredentials(code, password) {
+  let data = await fetch(`https://saraendpoint.azurewebsites.net/user/`);
+  //use string literals
+  let dataJson = await data.json();
+  console.log(dataJson);
+  console.log("user name:", code, "pass: ", password);
+  for (let i = 0; i < dataJson.length; i++) {
+    //console.log(`${i}`,dataJson[i].code,code,(dataJson[i].code).toString() === (code).toString())
+    //console.log(`${i}`,dataJson[i].name,password,dataJson[i].name === password);
+    if (
+      dataJson[i].code.toString() === code &&
+      dataJson[i].name.toString() === password
+    )
+      return true;
+  }
+  return false;
 }
---
+++ frontend/src/login-register/recoverpass.jsx
@@ -0,0 +1,21 @@
+// print recovery
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class RecoverPass extends Component {
+    render() {
+        return (
+            // poner hola
+
+            <div>
+              <h1>RecoverPass</h1>
+
+            </div>
+        );
+}
+}
+
+
+export default RecoverPass;
