ec9297b2a236e4d85dbdb0d5c6c9d0f8581c87f6
つばき_0x01 <santiago.ramirez.ospina@correounivalle.edu.co>
2023-05-10 13:51:27 -0500

    Solve db settings

--- core/settings.py
+++ core/settings.py
@@ -114,23 +114,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'xejxgcji', 
-#         'USER': 'xejxgcji', 
-#         'PASSWORD': 'ASpclb1GbR2wQqU7j4xiKEsovbpWsh3u',
-#         'HOST': 'babar.db.elephantsql.com', 
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'xejxgcji', 
+        'USER': 'xejxgcji', 
+        'PASSWORD': 'ASpclb1GbR2wQqU7j4xiKEsovbpWsh3u',
+        'HOST': 'babar.db.elephantsql.com', 
+        'PORT': '5432',
+    }
 }
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
