a725aab5abeb19d65fca91522c76780671a7db56
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-05-16 18:46:11 -0500

    token check url

--- Token_app/views.py
+++ Token_app/views.py
@@ -1,7 +1,6 @@
 from .serializer import Serializador_token
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.views import APIView
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 import jwt
