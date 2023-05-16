359633b7b1be5fb88c7df6315b326d480a572c90
EdinsonUwU <edinson.dorado@correounivalle.edu.co>
2023-05-16 18:15:46 -0500

    token check url

--- Token_app/views.py
+++ Token_app/views.py
@@ -1,5 +1,34 @@
 from .serializer import Serializador_token
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from rest_framework.views import APIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+import jwt
 class ObtainToken(TokenObtainPairView):
     serializer_class = Serializador_token
+
+'''
+Check Token es la funcion a la que llama la url /checkToken para revisar un token esta valido.
+En request, se espera que se mande un acces token.
+Si se quiere acceder a payload, solo revisar payload, que recibe el valor de jwt.decode
+'''
+class CheckToken(APIView):
+    def post (self,request):
+        print(request)
+        token = request.data['jwt']
+        
+        if not token:
+            return Response("Token no existe")
+
+        try:
+            payload = jwt.decode(token,SECRET_KEY,algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            return Response("Token invalido, token expirado")
+        except jwt.InvalidSignatureError:
+            return Response("Token invalido, firma no valida")
+        except:
+            return Response("Token invalido")
+
+        
+        return Response("Token válido")
